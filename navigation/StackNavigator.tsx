9a64c04390da7e3bcf9835eba725aee15bef98f1
SufriedJakub <sufriedjakub@icloud.com>
2025-07-08 22:08:10 +0200

    StackNavigator.tsx

---
+++ navigation/StackNavigator.tsx
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+import ProfileSetupScreen from '../screens/ProfileSetupScreen';
+import HomeScreen from '../screens/HomeScreen';
+import ChatScreen from '../screens/ChatScreen';
+import MatchScreen from '../screens/MatchScreen';
+
+const Stack = createNativeStackNavigator();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Screen name="ProfileSetup" component={ProfileSetupScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Chat" component={ChatScreen} />
+      <Stack.Screen name="Match" component={MatchScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default StackNavigator;
