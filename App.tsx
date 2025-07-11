cc4e75cc074272054292d5046a6a23f9957bb633
SufriedJakub <sufriedjakub@icloud.com>
2025-07-11 15:04:16 +0200

    App.tsx

--- App.tsx
+++ App.tsx
@@ -1,16 +1,7 @@
 
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import StackNavigator from './navigation/StackNavigator';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import AppNavigator from './navigation/AppNavigator';
 
 export default function App() {
-  return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <NavigationContainer>
-        <StackNavigator />
-      </NavigationContainer>
-    </GestureHandlerRootView>
-  );
+  return <AppNavigator />;
 }
-
