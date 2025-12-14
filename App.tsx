c8e73a6be5d2da53f847bfaaded5329a962ad87f
SufriedJakub <sufriedjakub@icloud.com>
2025-12-14 16:57:43 +0100

    App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
-
 import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler'; // <--- Ważny import
 import AppNavigator from './navigation/AppNavigator';
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    // Styl flex: 1 jest konieczny
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <AppNavigator />
+    </GestureHandlerRootView>
+  );
 }
